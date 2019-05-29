d1e288bdb73c0e8ac3c3fc19901b85604aeec618
Daniel Aguila <danielaguila_96@hotmail.com>
2019-05-29 10:53:23 -0300

    Cleaning code, add dev dependencies

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Thanos from "react-thanos";
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,6 +9,13 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles({
+  bodyBlack: {
+    backgroundColor: '#000',
+    height: '100vh'
+  },
+  bodyWhite: {
+    backgroundColor: '#fff',
+  },
   root: {
     padding: '2px 4px',
     display: 'flex',
@@ -32,27 +39,40 @@ const useStyles = makeStyles({
     right: 10,
     bottom: 5,
   },
+  app: {
+    margin: '40vh auto 0',
+    fontSize: 18,
+    transition: 'all .55s'
+  },
+  app_no_margin: {
+  }
 });
 
 function App() {
+  const [snap, setSnap] = useState(null);
   const classes = useStyles();
   function busca() {
     console.log("BUSCA!");
   };
 	return (
-		<div className='app'>
-      <Paper className={classes.root}>
-        <InputBase className={classes.input} placeholder="Busque aqui" />
-        <Divider className={classes.divider} />
-        <IconButton color="primary" className={classes.iconButton} aria-label="Directions">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-      <div className={classes.thanos}>
-        <Thanos
-          onSnap={() => busca()}
-          onSnapReverse={() => busca()}
-        />
+		<div className={snap ? classes.bodyBlack : classes.bodyWhite}>
+      <div className={snap ? classes.app_no_margin : classes.app}>
+          {
+            !snap &&
+              <Paper className={classes.root}>
+                <InputBase className={classes.input} placeholder="Busque aqui" />
+                <Divider className={classes.divider} />
+                <IconButton color="primary" className={classes.iconButton} aria-label="Directions">
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+          }
+        <div className={classes.thanos}>
+          <Thanos
+            onSnap={() => {setSnap(true); busca()}}
+            onSnapReverse={() => setSnap(false)}
+          />
+        </div>
       </div>
     </div>
 	);
