da4cfd6ab81ef4ef8b8f5c24935da170f5393773
danielaguiladev <jackdeniels96@gmail.com>
2019-06-20 17:23:26 -0300

    Updates

---
+++ precache-manifest.2a0222cfb556535ff683d690d1c08f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16a7bc229714d27b56b01145f9a8bfc",
+    "url": "/sistemasDistribuidos/index.html"
+  },
+  {
+    "revision": "778ee9f4832e9b2b60e8",
+    "url": "/sistemasDistribuidos/static/css/main.2e6a0298.chunk.css"
+  },
+  {
+    "revision": "fd1f9e6c1241b136408e",
+    "url": "/sistemasDistribuidos/static/js/2.b86445da.chunk.js"
+  },
+  {
+    "revision": "778ee9f4832e9b2b60e8",
+    "url": "/sistemasDistribuidos/static/js/main.001e7916.chunk.js"
+  },
+  {
+    "revision": "8a8f789d693b5e81c362",
+    "url": "/sistemasDistribuidos/static/js/runtime~main.a952df53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sistemasDistribuidos/precache-manifest.67e35b185791bb018a9f14fd82750fa4.js"
+  "/sistemasDistribuidos/precache-manifest.2a0222cfb556535ff683d690d1c08f34.js"
 );
 
 self.addEventListener('message', (event) => {
